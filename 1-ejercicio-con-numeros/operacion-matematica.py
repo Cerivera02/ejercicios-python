b10350a669b9b5392aa71d8e19e3cad69b9f6d61
Carlos Eduardo <cerivera2002@gmail.com>
2022-07-04 00:32:10 -0700

    Se agrego las operaciones internas y mostrar en pantalla

--- 1-ejercicio-con-numeros/operacion-matematica.py
+++ 1-ejercicio-con-numeros/operacion-matematica.py
@@ -3,3 +3,10 @@
 n1 = int (input("Ingresa el primer numero "))
 n2 = int (input("Ingresa el segundo numero "))
 
+suma = n1 + n2
+resta = n1 - n2
+mult = n1 *n2
+
+print("La suma es ", suma)
+print("La resta es ", resta)
+print("la multiplicacion es ", mult)
