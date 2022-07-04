e514413635497ef2f84034280511aab5b8e76431
Carlos Eduardo <cerivera2002@gmail.com>
2022-07-04 00:39:04 -0700

    Instrucciones del problema

---
+++ 2-Calcular-el-cambio/calcular-cambio.py
@@ -0,0 +1,5 @@
+#Construye un programa que, al recibir como datos el costo de un artuculo vendido y la cantidad de dinero entregada por el cliente calcula
+
+#1 el cambio que debe de entregar al cliente, si el pago efectuado es mayor al pproducto
+#2 que pasa si el cliete paga exactamente lo qeu cale el producto
+#la cantidad de dinero que falta por pafar, si el pago efectuado es menor que el precio del producto
