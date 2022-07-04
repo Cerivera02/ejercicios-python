7d03c7615abaa9852fc61424ebe4724cffcb800c
Carlos Eduardo <cerivera2002@gmail.com>
2022-07-04 00:22:24 -0700

    Introducir valores desde terminal directo a uan variable

---
+++ 1-ejercicio-con-numeros/operacion-matematica.py
@@ -0,0 +1,5 @@
+#Cree un programa que, al recibir como datos dos numeros enteros, calcule la suma, resta  multiplicacion de dichos numeros
+
+n1 = int (input("Ingresa el primer numero "))
+n2 = int (input("Ingresa el segundo numero "))
+
